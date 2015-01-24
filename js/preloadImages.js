82a1363c6f88c4d2c9cfe968682c6ef8cb767197
piaomiaoone <864747537@qq.com>
2015-01-24 23:34:48 +0800

    Create preloadImages.js
    jquery预加载图片

---
+++ js/preloadImages.js
@@ -0,0 +1,11 @@
+/*
+*jQuery 预加载图片
+*/
+
+jQuery.preloadImages = function() {
+    for(var i = 0; i < arguments.length; i++) {
+        $("<img />").attr('src', arguments[i]);
+    }
+};
+//用法
+$.preloadImages('image1.gif', '/path/to/image2.png', 'some/image3.jpg');
